86881fbafadc6b223ab97985f0297c557d433fac
HgrassM <119745077+HgrassM@users.noreply.github.com>
2024-06-27 20:43:12 -0300

    Implemented the movement functions

--- workspace/src/delivery_robot/src/bt_folder/bt_nodes.cpp
+++ workspace/src/delivery_robot/src/bt_folder/bt_nodes.cpp
@@ -1,18 +1,24 @@
 #include <iostream>
 #include <string>
-#include <thread>
+#include <mutex>
+#include <cmath>
 
 #include "behaviortree_cpp_v3/behavior_tree.h"
 #include "behaviortree_cpp_v3/action_node.h"
 #include "behaviortree_cpp_v3/bt_factory.h"
 #include "geometry_msgs/msg/Pose.hpp"
+#include "geometry_msgs/msg/Point.hpp"
+#include "geometry_msgs/msg/Quaternion.hpp"
 #include "nav_msgs/msg/Odometry.hpp"
 #include "sensor_msgs/msg/BatteryState.hpp"
 
 std::mutex global_mutex;
 double targetX = 0.0;
 double targetY = 0.0;
-sensor_msgs::msg::BatteryState batteryState;
+double target_angular = 0.0;
+geometry_msg::msg::Point current_position = NULL;
+geometry_msg::msg::Quaternion quaternion_data = NULL;
+sensor_msgs::msg::BatteryState batteryState = NULL;
 
 struct DeliveryInfo {
 	std::string food_id;
@@ -64,8 +70,7 @@ BT::NodeStatus BatteryStatus() {
 class RegisterDeliveryInfo : public BT::SyncActionNode {
 	public:
 		RegisterDeliveryInfo(const std::string& name, const BT::NodeConfiguration& config)
-		       : BT::SyncActionNode(name, config)
-		{}
+		       : BT::SyncActionNode(name, config) {}
 
 		static BT::PortsList providedPorts() {
 			return {BT::OutputPort<DeliveryInfo>("delivery_info_output")};
@@ -96,7 +101,7 @@ class RegisterDeliveryInfo : public BT::SyncActionNode {
 class GetRoomInfo : public BT::SyncActionNode {
 	public:
 		GetRoomInfo(const std::string& name, const BT::NodeConfiguration config)
-			: BT::SyncActionNode(name, config)
+			: BT::SyncActionNode(name, config) {}
 
 		static BT::PosrtsList providedPorts() {
 			return {BT::InputPort<DeliveryInfo>("delivery_info_input")};
@@ -114,4 +119,48 @@ class GetRoomInfo : public BT::SyncActionNode {
 			targetY = data.y;			
 		}
 };
- 
+
+class GoToPatientRoom : public BT::SyncActionNode {
+	private:
+		double current_linear_error_ = 0.0;
+		double current_angular_error_ = 0.0;
+		double linear_error_sum_ = 0.0;
+		double angular_error_sum_ = 0.0;
+		const double KP_ = 0.3;
+		const double KI_ = 0.1;
+
+		void calculate_error() {
+			double linear_error = sqrt(((targetX - current_position.x)**2) + (targetY - current_position.y)**2);
+			
+			double target_angle = atan2(targetY - current_position.y, targetX - current_position.x);
+			
+			global_mutex.lock();
+			double current_angle = atan2(2.0*(quaternion_data.y*quaternion_data.x + 
+					quaternion_data.w*quaternion_data.z), 1.0 - 2.0*(quaternion_data.z**2 +
+					quaternion_data.y**2));
+			global_mutex.unlock();
+
+			double angular_error = target_angle - current_angle;
+			
+			this -> current_linear_error_ = linear_error;
+			this -> current_angular_error_ = angular_error;
+			this -> linear_error_sum_ += linear_error;
+			this -> angular_error_sum_ += angular_error;
+		}
+
+		double calculate_linear_velocity() {
+			return (KP_*current_linear_error_) + (KI_*linear_error_sum_);
+		}
+
+		double calculate_angular_velocity() {
+			return (KP_*current_angular_error_) + (KI_*angular_error_sum_);
+		}
+	
+	public:
+		GoToPatientRoom(const std::string& name)
+			: BT::SyncActionNode(name, {}) {}
+		
+		BT::NodeStatus tick() override {
+			//TODO terminar de implementar o PID	
+		}
+};
